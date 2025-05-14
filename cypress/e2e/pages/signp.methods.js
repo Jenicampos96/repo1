88eaac46442834e3a2dc0358218365f0f84e0ec6
Jenicampos96 <jencamb@gmail.com>
2025-05-13 20:31:30 -0600

    creacion de pagina de signup

---
+++ cypress/e2e/pages/signp.methods.js
@@ -0,0 +1,21 @@
+import { SignupElements } from "./signup/signup.elements";
+
+export class SignupMethods {
+    static insertUSerNAme(username){
+        SignupElements.textboxes.username.invoke("val",username);
+    }
+    
+    static insertPassword(password){
+        SignupElements.textboxes.password.invoke("val",password);
+    }
+
+    static clickOnsingupBoton(){
+        SignupElements.buttons.signup.click()
+    }
+
+    static signup(username, password){
+        this.insertUSerNAme(username);
+        this.insertPassword(password);
+        this.clickOnsingupBoton();
+    }
+}
