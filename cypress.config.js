b754125e5f8e2871bb5afb0e6e4a014a37febd25
Jenicampos96 <jencamb@gmail.com>
2025-05-15 16:38:48 -0600

    eliminar video despues de la ejecusion

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,9 @@ const { defineConfig } = require("cypress");
 const allureWriter = require('@shelex/cypress-allure-plugin/writer');
 
 module.exports = defineConfig({
+  retries:1,
+  video: false,
+  
   e2e: {
     setupNodeEvents(on, config) {
       allureWriter(on, config);
